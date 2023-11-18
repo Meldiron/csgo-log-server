a41664f80deaee89d39740c35a60b557f220262c
Matej Bačo <matejbaco2000@gmail.com>
2023-11-18 20:08:30 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,19 +1,19 @@
-import { Client } from 'node-appwrite';
+import { Client, Databases } from 'node-appwrite';
 
 // This is your Appwrite function
 // It's executed each time we get a request
 export default async ({ req, res, log, error }) => {
-  log(req.headers);
-  log(req.bodyRaw);
-  log(req.method);
-  log(req.url);
+  const client = new Client()
+     .setEndpoint('https://cloud.appwrite.io/v1')
+     .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
+     .setKey(process.env.APPWRITE_API_KEY);
 
-  // Why not try the Appwrite SDK?
-  //
-  // const client = new Client()
-  //    .setEndpoint('https://cloud.appwrite.io/v1')
-  //    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-  //    .setKey(process.env.APPWRITE_API_KEY);
+  const databases = new Databases(client);
+
+  await databases.createDocument('main, 'logs', 'unique()', {
+    log: req.bodyRaw,
+    logId: req.path
+  });
 
   return res.send('OK');
 };
